f62d1cf052d2a154288fecf16954a1c613bc6623
Erik Shea <hoopsnake@gmail.com>
2020-10-02 00:53:02 +0200

    Simplified classes

--- rock-paper-scissors-game/src/rps/RPSPlayer.java
+++ rock-paper-scissors-game/src/rps/RPSPlayer.java
@@ -1,52 +1,52 @@
 package rps;
 
 public class RPSPlayer {
-	private int score;
-	private int hand;
-	private String lastOutcome;
-	
-	public RPSPlayer()
-	{
-		this.score=0;
+	private int score; 	// Current score, starts at 0 and increments with each win
+	private int hand; 	// Current hand shape, in the form of a number between 1 and 3
+	private String lastOutcome;	// Last game outcome, eg. "Gagné".
+
+	public RPSPlayer() {
+		this.score = 0; // TODO: see if can get rid of
 	}
-	
-	public void play(RPSPlayer opponent)
-	{
-		if ( this.hand == opponent.getHand() ) 					// Draw.
+
+	/**
+	 * Determines if the current hand beats an opponent's current hand. Changes both
+	 * players' relevant parameters according to the outcome.
+	 * 
+	 * @param opponent to play against
+	 */
+	public void play(RPSPlayer opponent) {
+		if (this.hand == opponent.getHand())					// Draw.
 		{
-			this.draw(opponent);								// Win conditions:
-		} else if (	this.hand == 1 && opponent.getHand() == 3	// -rock vs. scissors
-				|| 	this.hand == 2 && opponent.getHand() == 1	// -paper vs. rock
-				|| 	this.hand == 3 && opponent.getHand() == 2 )	// -scissors vs. paper
+			this.lastOutcome = opponent.lastOutcome = "Match nul";
+		} else if (this.hand == 1 && opponent.getHand() == 3 	// Win if rock vs. scissors
+				|| this.hand == 2 && opponent.getHand() == 1 	// 	   or paper vs. rock
+				|| this.hand == 3 && opponent.getHand() == 2) 	//     or scissors vs. paper
 		{
-			this.win(opponent);
+			this.winsAgaint(opponent);
 		} else 													// Loss otherwise
 		{
-			opponent.win(this);
+			opponent.winsAgaint(this);
 		}
 	}
-	
-	public void win(RPSPlayer opponent)
-	{
-		this.incrementScore();
-		this.lastOutcome = "Gagné";
-		
-		opponent.setLastOutcome("Perdu");
-	}
-	
-	public void draw(RPSPlayer opponent)
-	{
-		this.lastOutcome = "Match nul";
-		opponent.setLastOutcome( this.lastOutcome );
-	}
-	
+
 	/**
-	 * Converts a choice in the form of a number to its corresponding string.
+	 * Handle parameter changes when a win occurs.
 	 * 
-	 * @param choice int between 1 and 3
-	 * @return "pierre" if choice is 1, "papier" if 2, "ciseaux" otherwise
+	 * @param opponent who lost
 	 */
-	public String getHandString()
+	private void winsAgaint(RPSPlayer opponent) {
+		this.score++;
+		this.lastOutcome = "Gagné";
+		opponent.lastOutcome = "Perdu";
+	}
+
+	/**
+	 * Returns string corresponding to current hand (eg. 1 returns "pierre").
+	 * 
+	 * @return String equivalent of hand number
+	 */
+	public String getHandString() // TODO: see if possible to share hand names with setHand(string)
 	{
 		String handString;
 
@@ -60,62 +60,49 @@ public class RPSPlayer {
 
 		return handString;
 	}
-	
-	public void setRandomHand()
-	{
-		this.hand = (int) (3 * Math.random() + 1);
+
+	/**
+	 * Sets hand at random
+	 */
+	public void setRandomHand() {
+		this.hand = (int) (3 * Math.random() + 1); // random int from 1 to 3 inclusive
 	}
-	
-	private void incrementScore()
-	{
-		this.score++;
-	}
-	
+
+
 	/** SETTERS **/
-	
-	public void setLastOutcome(String outcome)
-	{
-		this.lastOutcome = outcome;
-	}
-	
-	public String getLastOutcome()
-	{
-		return this.lastOutcome;
-	}
-	
-	public void setHand(int hand)
-	{
-		this.hand = hand;
-	}
-	
-	public int getHand()
-	{
+
+	public int getHand() {
 		return this.hand;
 	}
-	
-	public int getScore()
-	{
+
+	public int getScore() {
 		return this.score;
 	}
 	
+	public String getLastOutcome() {
+		return this.lastOutcome;
+	}
+	
+	public void setHand(int hand) {
+		this.hand = hand;
+	}
+	
 	/**
-	 * Asks user to enter one of three options: "pierre", "papier", or "ciseaux".
-	 * If anything else is entered, keeps asking.
-	 * 
-	 * @return int : 1 for "pierre", 2 for "papier", 3 for "ciseaux"
+	 * Set hand using its name (eg. "pierre" becomes 1). Only looks at the first two
+	 * letters.
 	 */
-	public void setHand(String handString) throws IllegalArgumentException
-	{
-		handString = handString.toLowerCase();
-		
+	public void setHand(String handString) throws IllegalArgumentException {
+		handString = handString.toLowerCase();	// Ignore capitalization
+
 		if (handString.startsWith("pi")) {
 			this.hand = 1;
 		} else if (handString.startsWith("pa")) {
 			this.hand = 2;
 		} else if (handString.startsWith("ci")) {
 			this.hand = 3;
-		} else { // If none of these three, ask again.
-			throw new IllegalArgumentException ("Main invalide: " + handString);
+		} else { // If none of these three, invalid parameter.
+			throw new IllegalArgumentException("Nom de main invalide: " + handString);
 		}
 	}
+	
 }
